dedc6d6f0121b65b8b51d33d3da14e7b8b45463a
nbirame <biramendiaye71@gmail.com>
2024-01-22 09:20:17 +0000

    Correct code

--- __manifest__.py
+++ __manifest__.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Vacances",
+    'name': "Congés",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
 
     'description': """
-        Long description of module's purpose
+        Module de gestion des Congés
     """,
 
     'author': "Birame NDIAYE",
-    'website': "http://www.yourcompany.com",
+    'website': "https://nbirameblog.odoo.com/",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Hr',
     'version': '15.0.1',
 
     # any module necessary for this one to work correctly
