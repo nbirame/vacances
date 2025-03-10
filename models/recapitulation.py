e4786266fa84953b767301ae3e43cfec3862eace
nbirame <biramendiaye71@gmail.com>
2025-03-10 11:35:56 +0000

    correct code

--- models/recapitulation.py
+++ models/recapitulation.py
@@ -2,7 +2,7 @@ from odoo import models, fields, api
 
 
 class Recapitulation(models.Model):
-    _name="vacance.recaputilation"
+    _name="vacance.recapitulation"
     _description="Tableau de bord des congé"
 
     allocated_days = fields.Float(
