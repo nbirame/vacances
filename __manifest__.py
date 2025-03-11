8ccbc9018c6abc4f45cb97e6ae5e7f36fb0acde0
nbirame <biramendiaye71@gmail.com>
2025-03-11 10:47:25 +0000

    correct code

--- __manifest__.py
+++ __manifest__.py
@@ -35,7 +35,7 @@
         'views/party_view.xml',
         'views/views.xml',
         'views/dashbord_view.xml',
-        'views/recapitulation_view.xml',
+        'views/dashbord_conge_view.xml',
         # 'views/agent_view.xml',
         # 'views/leave_template.xml',
     ],
